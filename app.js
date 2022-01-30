a6b3c5d229ca6d2ab5d8117a961b2a5f2b7e6c5c
Siddhartha Rana <rana.siddharth994@gmail.com>
2022-01-30 10:40:49 +0530

    image upload/ multer

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 // connecting to database server 
 const mongoose = require('mongoose');
@@ -59,6 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));// helps to serve static
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
